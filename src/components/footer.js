b41331aea4b71eec3ba34e990259ae3537d5575e
deannapi <deannapi.mart@gmail.com>
2020-11-18 13:28:57 -0600

    added social to footer and dropdown effects

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,25 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <img src={logo_2} height="75px" alt="logo" />
+        <div>
+          <img src={logo_2} height="50px" alt="logo" />
+          <ul>
+            <li className="instagram">
+              <a href="https://www.instagram.com/restore_massageandbodywork?igshid=1aqi3wfvv97u5">
+                <i
+                  className="fa fa-instagram"
+                  id="insta"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </li>
+            <li className="facebook">
+              <a href="https://www.facebook.com/AllysiaMartinezLMT/">
+                <i className="fa fa-facebook"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </footer>
     );
   }
--- src/components/header.js
+++ src/components/header.js
@@ -14,52 +14,66 @@ export default function Nav(props) {
   return (
     <>
       <header>
-        <img src={logo_1} alt="logo" height="100px" className="header_logo" />
+        <nav>
+          <img src={logo_1} alt="logo" height="150px" className="header_logo" />
+          <div className="dropdown">
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Menu
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <ul>
+                <li className={`${ContactSelected && `navActive`}`}>
+                  <a href="#contact">
+                    <span
+                      onClick={() => {
+                        setContactSelected(true);
+                        setAppointmentsSelected(false);
+                        setHomeSelected(false);
+                      }}
+                    >
+                      {" "}
+                      Contact Me
+                    </span>
+                  </a>
+                </li>
+                <li className={` ${AppointmentsSelected && `navActive`}`}>
+                  <a href="#appointments">
+                    <span
+                      onClick={() => {
+                        setContactSelected(false);
+                        setAppointmentsSelected(true);
+                        setHomeSelected(false);
+                      }}
+                    >
+                      Appointments
+                    </span>
+                  </a>
+                </li>
+                <li className={` ${HomeSelected && `navActive`}`}>
+                  <a href="#home">
+                    <span
+                      onClick={() => {
+                        setContactSelected(false);
+                        setAppointmentsSelected(false);
+                        setHomeSelected(true);
+                      }}
+                    >
+                      Home
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </header>
-      <nav>
-        <ul>
-          <li className={`${ContactSelected && `navActive`}`}>
-            <a href="#contact">
-              <span
-                onClick={() => {
-                  setContactSelected(true);
-                  setAppointmentsSelected(false);
-                  setHomeSelected(false);
-                }}
-              >
-                {" "}
-                Contact Me
-              </span>
-            </a>
-          </li>
-          <li className={` ${AppointmentsSelected && `navActive`}`}>
-            <a href="#appointments">
-              <span
-                onClick={() => {
-                  setContactSelected(false);
-                  setAppointmentsSelected(true);
-                  setHomeSelected(false);
-                }}
-              >
-                Appointments
-              </span>
-            </a>
-          </li>
-          <li className={` ${HomeSelected && `navActive`}`}>
-            <a href="#home">
-              <span
-                onClick={() => {
-                  setContactSelected(false);
-                  setAppointmentsSelected(false);
-                  setHomeSelected(true);
-                }}
-              >
-                Home
-              </span>
-            </a>
-          </li>
-        </ul>
-      </nav>
     </>
   );
 }
