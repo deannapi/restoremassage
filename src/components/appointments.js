d4571bb887add12b9fc14f71fab04527b649e614
deannapi <deannapi.mart@gmail.com>
2020-11-20 08:24:46 -0600

    appointment page done

--- src/components/appointments.js
+++ src/components/appointments.js
@@ -1,29 +1,39 @@
 import React from "react";
+import blading from "../images/blade2.jpg";
+import backcup from "../images/backcup2.jpg";
 
-export default class Footer extends React.Component {
+export default class Appointments extends React.Component {
   render() {
     return (
       <>
-        <div>
-          <h3>Existing Clients </h3>
-          <a
-            href="https://square.site/book/S3JQTRESFMDNN/restore-massage-bodywork-san-antonio-tx"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Book an Appointment
-          </a>
-        </div>
+        <div className="row">
+          <div className="card">
+            <img src={blading} alt="blading" />
+            <div className="card-body">
+              <h5>Existing Clients</h5>
+              <a
+                href="https://square.site/book/S3JQTRESFMDNN/restore-massage-bodywork-san-antonio-tx"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Book an Appointment
+              </a>
+            </div>
+          </div>
 
-        <div>
-          <h3>New Client</h3>
-          <a
-            href="https://forms.office.com/Pages/ResponsePage.aspx?id=-WVLKyRff0ubDsCCr8W6C9wJOPw3nfpCoe5N78DwuPJUMzJIVEIzTFVUWVNOOUNFSkdUNzExWVNKNi4u"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Please complete the new client form before booking appointments.
-          </a>
+          <div className="card">
+            <img src={backcup} alt="blading" />
+            <div className="card-body">
+              <h5>New Clients</h5>
+              <a
+                href="https://forms.office.com/Pages/ResponsePage.aspx?id=-WVLKyRff0ubDsCCr8W6C9wJOPw3nfpCoe5N78DwuPJUMzJIVEIzTFVUWVNOOUNFSkdUNzExWVNKNi4u"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Please complete the new client form before booking appointments.
+              </a>
+            </div>
+          </div>
         </div>
       </>
     );
