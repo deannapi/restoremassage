bed51b75329986d594662911feac3229062d8f4a
deannapi <deannapi.mart@gmail.com>
2022-02-08 15:41:05 -0600

    updated header nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo_1 from "../images/Restore_Logo_Final trimmed.png";
+// import logo_1 from "../images/Restore_Logo_Final trimmed.png";
 
 export default function Nav(props) {
   return (
     <>
       <header>
         <nav>
-            <img
+          {/* <img
               src={logo_1}
               alt="logo"
               height="150px"
@@ -61,7 +61,43 @@ export default function Nav(props) {
                   </li>
                 </ul>
               </div>
-            </div>
+            </div> */}
+          <div className="bg-img">
+            <ul className="nav justify-content-end">
+              <li className="nav-item">
+                <Link to="/home">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/about">About Me</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact">Contact Me</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/appointments">Appointments</Link>
+              </li>
+            </ul>
+            <ul className="nav justify-content-end">
+              <li className="nav-item">
+                <Link to="/menu">Sessions & Prices</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/products">Products</Link>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="https://squareup.com/gift/RFMDW8TJJ8M7K/order?external_source=postoffice-email&external_token=ryEMlwN1ZO4mOH0n"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Gift Cards
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link to="/blog">Blog</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
     </>
