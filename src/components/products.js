fbfdb5e06912ba61650ab3b5a6cbe9ddba99437a
deannapi <deannapi.mart@gmail.com>
2023-12-20 10:05:11 -0600

    removed cbd prods

--- src/components/products.js
+++ src/components/products.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import MgCBDLav2oz from "../shop-items/MgCBDLav2oz";
-import MgCBDPpmt2oz from "../shop-items/MgCBDPpmt2oz";
-import MgCBDUns2oz from "../shop-items/MgCBDUns2oz";
-import MgCBDVan2oz from "../shop-items/MgCBDVan2oz";
-import MgCBDLav4oz from "../shop-items/MgCBDLav4oz";
-import MgCBDPpmt4oz from "../shop-items/MgCBDPpmt4oz";
-import MgCBDVan4oz from "../shop-items/MgCBDVan4oz";
+// import MgCBDLav2oz from "../shop-items/MgCBDLav2oz";
+// import MgCBDPpmt2oz from "../shop-items/MgCBDPpmt2oz";
+// import MgCBDUns2oz from "../shop-items/MgCBDUns2oz";
+// import MgCBDVan2oz from "../shop-items/MgCBDVan2oz";
+// import MgCBDLav4oz from "../shop-items/MgCBDLav4oz";
+// import MgCBDPpmt4oz from "../shop-items/MgCBDPpmt4oz";
+// import MgCBDVan4oz from "../shop-items/MgCBDVan4oz";
 import MgVan4oz from "../shop-items/MgVan4oz";
 import MgPpmt4oz from "../shop-items/MgPpmt4oz";
 import MgUnscent4oz from "../shop-items/MgUnscent4oz";
@@ -14,7 +14,7 @@ import MgVan9oz from "../shop-items/MgVan9oz";
 import MgLav9oz from "../shop-items/MgLav9oz";
 import MgPpmt9oz from "../shop-items/MgPpmt9oz";
 import MgUnscent9oz from "../shop-items/MgUnscent9oz";
-import FaceCBDOil from "../shop-items/FaceCBDOil";
+// import FaceCBDOil from "../shop-items/FaceCBDOil";
 import FaceOil from "../shop-items/FaceOil";
 import FaceScrub from "../shop-items/MgScrub";
 import FaceScrub8 from "../shop-items/MgScrub8";
@@ -169,13 +169,13 @@ export default class Products extends React.Component {
         </div>
 
         <div className="row">
-          <MgCBDLav2oz></MgCBDLav2oz>
+          {/* <MgCBDLav2oz></MgCBDLav2oz>
           <MgCBDPpmt2oz></MgCBDPpmt2oz>
           <MgCBDUns2oz></MgCBDUns2oz>
           <MgCBDVan2oz></MgCBDVan2oz>
           <MgCBDLav4oz></MgCBDLav4oz>
           <MgCBDPpmt4oz></MgCBDPpmt4oz>
-          <MgCBDVan4oz></MgCBDVan4oz>
+          <MgCBDVan4oz></MgCBDVan4oz> */}
           <MgVan4oz></MgVan4oz>
           <MgPpmt4oz></MgPpmt4oz>
           <MgUnscent4oz></MgUnscent4oz>
@@ -183,7 +183,7 @@ export default class Products extends React.Component {
           <MgLav9oz></MgLav9oz>
           <MgPpmt9oz></MgPpmt9oz>
           <MgUnscent9oz></MgUnscent9oz>
-          <FaceCBDOil></FaceCBDOil>
+          {/* <FaceCBDOil></FaceCBDOil> */}
           <FaceOil></FaceOil>
           <FaceScrub></FaceScrub>
           <FaceScrub8></FaceScrub8>
