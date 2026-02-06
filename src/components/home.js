50962689f58d2f187e5262d63123dd09dd7068e7
DeAnna Barker <deannapi.mart@gmail.com>
2026-02-06 16:08:12 -0600

    updated home title

--- src/components/home.js
+++ src/components/home.js
@@ -86,9 +86,10 @@ export default class Home extends React.Component {
       <>
         <div className="home">
           <div className="home-title">
-            <h1>San Antonio Sports Massage Therapy</h1>
-            <h4>Licensed Massage Therapist</h4>
-            <h4>Cupping Therapy</h4>
+            <h1>Bodywork &#8226; Massage &#8226; Stretch Therapy</h1>
+            <h4>Move Better &#8226; Recover Smarter &#8226; Live Stronger</h4>
+            {/* <h4>Recover smarter</h4> */}
+            {/* <h4>Live stronger</h4> */}
           </div>
 
           <div className="row">
